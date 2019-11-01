f4d283421d0e46afa6792faa91418ec8da6c9bae
WxrPro97 <ampesandupa@gmail.com>
2019-11-01 16:02:50 +0000

    rake-nltk and forms

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, request, render_template
 from flask_bootstrap import Bootstrap
+from rake_nltk import Rake
+import re
 
 app = Flask(__name__)
 Bootstrap(app)
@@ -7,6 +9,16 @@ Bootstrap(app)
 @app.route("/")
 def user_form():
     return render_template("index.html")
+
+@app.route("/", methods=['POST'])
+def user_result():
+    review = request.form['review']
+    r = Rake()
+    r.extract_keywords_from_text(review)
+    score = str(r.get_ranked_phrases_with_scores())
+    score = [float(s) for s in re.findall(r'-?\d+\.?\d*', score)]
+    total_score = sum(score)
+    return render_template('result.html', total_score=total_score)
 
 if __name__ == "__main__":
     app.run(debug=True)
