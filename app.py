3c5548bec842c7957202d95d9eefda67b4506d00
WxrPro97 <ampesandupa@gmail.com>
2019-11-22 15:22:03 +0000

    static login page

--- app.py
+++ app.py
@@ -1,16 +1,37 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect, url_for, session, abort
 from flask_bootstrap import Bootstrap
 from rake_nltk import Metric, Rake
 import re
-import statistics
+import os
 
 app = Flask(__name__)
 Bootstrap(app)
 
 
+@app.route('/login', methods=['POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            session['logged_in'] = True
+            return redirect(url_for('user_form'))
+    return render_template('login.html', error=error)
+
+
+@app.route("/logout")
+def logout():
+    session['logged_in'] = False
+    return login()
+
+
 @app.route("/")
 def user_form():
-    return render_template("index.html")
+    if not session.get('logged_in'):
+        return render_template('login.html')
+    else:
+        return render_template("index.html")
 
 
 @app.route("/", methods=['POST'])
@@ -28,7 +49,8 @@ def user_result():
     r = Rake(stopwords=None, punctuations=';')
     r.extract_keywords_from_sentences(install)
     install_score = str(r.get_ranked_phrases_with_scores())
-    install_score = [float(s) for s in re.findall(r'-?\d+\.?\d*', install_score)]
+    install_score = [float(s)
+                     for s in re.findall(r'-?\d+\.?\d*', install_score)]
     install_total_score = sum(install_score)
 
     # Functionality
@@ -47,8 +69,10 @@ def user_result():
     exp_score = [float(s) for s in re.findall(r'-?\d+\.?\d*', exp_score)]
     exp_total_score = sum(exp_score)
 
-    return render_template('result.html', perf_total_score=perf_total_score, install_total_score=install_total_score, func_total_score = func_total_score, exp_total_score = exp_total_score)
+    return render_template('result.html', perf_total_score=perf_total_score, install_total_score=install_total_score, func_total_score=func_total_score, exp_total_score=exp_total_score)
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=80)
+    app.secret_key = os.urandom(12)
+    app.run(debug=True)
+    # host="0.0.0.0", port=80
