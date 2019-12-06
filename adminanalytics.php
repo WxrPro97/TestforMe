c58600eea52e04659460a80ba2d555431f059c01
JeganFM <jeganmendy25@gmail.com>
2019-12-06 01:56:04 +0000

    added progress bar correctly

--- adminanalytics.php
+++ adminanalytics.php
@@ -191,75 +191,63 @@
   </section>
   <!-- end of cards -->
   <section>
-        <div class="container-fluid">
-            <div class="row mb-5">
-                <div class="col-xl-10 col-lg-9 col-md-9 ml-auto">
-                    <div class="row">
-                        <div class="col-12">
-
-                            <div class="card shadow">
-                                <div class="card-header py-6">
-                                    <p class="text-primary m-0 font-weight-bold">Jobs Info</p>
-                                </div>
-                                <div class="row">
-                            <div class="col-sm-8 col-md-8 col-lg-8 mx-auto">
-                                <div class="card card-signin my-5">
-                                    <div class="card-body">
-                                        <h5 class="card-title text-center">Submit Review</h5>
-                                        <hr class="my-4">
-
-                                        <form action="testprojects.php" method="post">
-                                            <div class="form-group">
-                                                <label>Company Name</label>
-                                                <input type="text" name="company" class="form-control">
-
+           <div class="container-fluid">
+               <div class="row mb-5">
+                   <div class="col-xl-10 col-lg-9 col-md-8 ml-auto">
+                       <div class="row">
+                           
+                                <div class="col-12">
+                                        <h3 class="text-dark mb-4">Review Score
+                                        </h3>
+                                        <div class="card shadow">
+                                            <div class="card-header py-3">
+                                                <p class="text-primary m-0 font-weight-bold">Review Score</p>
                                             </div>
-
-                                            <div class="form-group">
-                                                <label>Product Name</label>
-                                                <input type="text" name="product" class="form-control">
-                                            </div>
-
-                                            <div class="form-group">
-                                                <label>Test Type</label>
-                                                <select type="text" name="testtype" class="form-control">
-                                                    <option selected>Select test type</option>
-                                                    <option value="Alpha">Alpha</option>
-                                                    <option value="Beta">Beta</option>
-                                                </select>
-                                            </div>
-
-                                            <div class="form-group">
-                                                <label>Software Type</label>
-                                                <input Type="text" name="softwaretype" class="form-control">
-                                            </div>
-
-                                            <div class="form-group">
-                                                <label>Deadline</label>
-                                                <div class="col-lg-78 text-center">
-                                                    <div id="success"></div><input class="form-control" type="date" name="deadline">
-                                                </div>
-                                            
-                                            </div>
-
-                                            <div class="form-group">
-                                                <label>Link</label>
-                                                <input type="text" name="link" class="form-control">
-                                            </div>
-
-
-                                            <button class="btn btn-lg btn-primary btn-block text-uppercase" name="submit"
-                                                type="submit" value="submit">Submit</button>
-                                            <hr class="my-4">
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
+                                            <div class="card-body">
+                                                    <h4 class="small font-weight-bold">
+                                                            Server migration
+                                                            <span class="float-right">20%</span>
+                                                        </h4>
+                                                        <div class="progress mb-4">
+                                                            <div class="progress-bar bg-danger" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100" style="width: 20%;">
+                                                               <span class="sr-only">20%</span>
+                                                           </div>                         
+                                                        </div>
+                                                        <h4 class="small font-weight-bold">
+                                                            Server migration
+                                                            <span class="float-right">40%</span>
+                                                        </h4>
+                                                        <div class="progress mb-4">
+                                                            <div class="progress-bar bg-success" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: 40%;">
+                                                                <span class="sr-only">40%</span>
+                                                            </div>                         
+                                                        </div>
+                                                        <h4 class="small font-weight-bold">
+                                                            Tracking
+                                                            <span class="float-right">60%</span>
+                                                        </h4>
+                                                        <div class="progress mb-4">
+                                                            <div class="progress-bar bg-warning" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%;">
+                                                                <span class="sr-only">60%</span>
+                                                            </div>
+                                                        </div>
+                                                        <h4 class="small font-weight-bold">
+                                                            Database
+                                                            <span class="float-right">80%</span>
+                                                        </h4>
+                                                        <div class="progress md-4">
+                                                            <div class="progress-bar bg-primary" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100" style="width: 80%";>
+                                                                <span class="sr-only">80%</span>
+                                                            </div>
+                                                        </div>
+                                        </div>
+                                    </div> 
+                           </div>
+                       </div>
+                   </div>
+               </div>
+           </div>
+       </section>
 
 
 </body>
